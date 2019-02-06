86373968a38dd39858814548b4fdb029c13738f7
MuravlevAS <43071478+Muravchik@users.noreply.github.com>
2019-02-06 12:38:09 +0700

    placing cube

--- colorcontrol/src/main/java/ru/sgk/colorcontrol/game/GameCube.java
+++ colorcontrol/src/main/java/ru/sgk/colorcontrol/game/GameCube.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 
-import io.netty.handler.codec.AsciiHeadersEncoder.NewlineType;
 import ru.sgk.colorcontrol.MainColorControl;
 
 public class GameCube 
@@ -55,7 +55,7 @@ public class GameCube
 		this.z = (int) PlayerLoc.getZ();
 
 		PlayerLoc.setY(y);
-		place(PlayerLoc);
+		place(PlayerLoc, team);
 		
 		this.cubeTeam = team;
 	}
@@ -95,25 +95,67 @@ public class GameCube
 	{
 		isBase = b;
 	}
+
+	private void place(Location loc, GameTeam team)
+	{
+		
+	}
 	
+	@SuppressWarnings("deprecation")
 	private void place(Location loc)
 	{
-		min = loc;
-		max = loc;
-		min.setX((int)(loc.getX()) - 2);
-		min.setY((int)(loc.getY()) - 5);
-		min.setZ((int)(loc.getZ()) - 2);
-		max.setX((int)(loc.getX()) + 2);
-		max.setY((int)(loc.getY()) - 1);
-		max.setZ((int)(loc.getZ()) + 2);
+		min = new Location(loc.getWorld(), x - 2, y - 5, z - 3);
+		max = new Location(loc.getWorld(), x + 2, y - 1, z + 1);
+//		min.setX((int)(loc.getX()) - 2);
+//		min.setY((int)(loc.getY()) - 5);
+//		min.setZ((int)(loc.getZ()) - 2);
+//		max.setX((int)(loc.getX()) + 2);
+//		max.setY((int)(loc.getY()) - 1);
+//		max.setZ((int)(loc.getZ()) + 2);
+		
+		MainColorControl.debugMessage("§cmin x: " + min.getX());
+		MainColorControl.debugMessage("§cmax x: " + max.getX());
+		MainColorControl.debugMessage("§cmin y: " + min.getY());
+		MainColorControl.debugMessage("§cmax y: " + max.getY());
+		MainColorControl.debugMessage("§cmin z: " + min.getZ());
+		MainColorControl.debugMessage("§cmax z: " + max.getZ());
 		
 		if (!isRegionEmpty(min, max))
 		{
 			MainColorControl.debugMessage("§cВ области уже есть блоки. Нужно поставить куб в другое место");
 			return;
 		}
-		
-		
+
+		Location placeLoc = min;
+		for (int x = min.getBlockX(); x <= max.getBlockX(); x++) 
+		{
+			MainColorControl.debugMessage("§aУстановка новой координаты x: " + x);
+			for (int y = min.getBlockY(); y <= max.getBlockY(); y++) 
+			{
+				MainColorControl.debugMessage("§aУстановка новой координаты y: " + y);
+				for (int z = min.getBlockZ(); z <= max.getBlockZ(); z++) 
+				{
+					MainColorControl.debugMessage("§aУстановка новой координаты z: " + z);
+					placeLoc = new Location(loc.getWorld(), x, y, z);
+
+					MainColorControl.debugMessage("§aСтавим блок на координаты §c" + placeLoc.getX() + "" + placeLoc.getY() + "" + placeLoc.getZ());
+					if (((x == min.getBlockX() || x == max.getBlockX()) && (y == min.getBlockY() || y == max.getBlockY())) ||
+						((z == min.getBlockZ() || z == max.getBlockZ()) && (y == min.getBlockY() || y == max.getBlockY())) ||
+						((x == min.getBlockX() || x == max.getBlockX()) && (z == min.getBlockZ() || z == max.getBlockZ())))
+					{
+						MainColorControl.debugMessage("§aУстановка рёбер куба");
+						placeLoc.getBlock().setType(Material.CONCRETE);
+						placeLoc.getBlock().setData((byte)15);
+					}
+					else
+					{
+						MainColorControl.debugMessage("§aУстановка граней куба");
+						placeLoc.getBlock().setType(Material.CONCRETE);
+					}
+				}
+			}
+		}
+		MainColorControl.debugMessage("§aКуб поставлен");
 	}
 	
 	private boolean isRegionEmpty(Location min, Location max)
@@ -128,11 +170,13 @@ public class GameCube
 					loc1 = new Location(min.getWorld(), i, h, j);
 					if (!loc1.getBlock().isEmpty())
 					{
+						MainColorControl.debugMessage("§rКуб не может быть размещён здесь, т.к область занята");
 						return false;
 					}
 				}
 			}
 		}
+		MainColorControl.debugMessage("§aОбласть свободна, установка куба...");
 		return true;
 	}
 	
